2e873f8977f2b6fd554860e0c3c6092787bb5ea0
david <david.milov@gmail.com>
2024-08-25 20:48:36 +0300

    update backend

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { query } = require("express");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -22,7 +23,7 @@ async function getRecipeInformation(recipe_id) {
 
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
-    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
+    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, serving, ingredients } = recipe_info.data;
 
     return {
         id: id,
@@ -33,6 +34,8 @@ async function getRecipeDetails(recipe_id) {
         vegan: vegan,
         vegetarian: vegetarian,
         glutenFree: glutenFree,
+        serving: serving,
+        ingredients: ingredients,
         
     }
 }
@@ -48,13 +51,201 @@ async function searchRecipe(recipeName, cuisine, diet, intolerance, number, user
             apiKey: process.env.spooncular_apiKey
         }
     });
+    console.log("recipeName : " + recipeName + " cuisine : " + cuisine +
+       " diet : " + diet + " intolerance : " + intolerance + " number : " + number + " username : " + username)
+    console.log("here in search recipe function in recipes_utils.js");
+    console.log(response.data.results.map((element) => element.id), username);
+
 
     return getRecipesPreview(response.data.results.map((element) => element.id), username);
 }
+async function getRecipesPreviewFromDb(recipes_ids_list) {
+    let promises = [];
+    recipes_ids_list.map((id) => {
+        promises.push(getRecipeInformationFromDb(id));
+    });
+    let info_res = await Promise.all(promises);
+    info_res = extractPreviewRecipeDetails(info_res);
+    //info_res.map((recp)=>{console.log(recp.data)});
+    
+    console.log(info_res);
+    return info_res
+}
+
+async function getRecipeInformationFromDb(recipe_id) {
+    const query = `
+      SELECT 
+          recipe_id,
+          user_id,
+          title,
+          ingredients,
+          instructions,
+          servings,
+          time AS readyInMinutes,
+          likes,
+          vegan,
+          gluten_free AS glutenFree,
+          vegetarian,
+          image
+      FROM 
+          recipes
+      WHERE 
+          recipe_id = ${mysql.escape(recipe_id)};
+    `;
+  
+    const result = await DButils.execQuery(query);
+    
+    if (result.length === 0) {
+      throw new Error("Recipe not found");
+    }
+  
+    const recipe = result[0];
+    recipe.ingredients = recipe.ingredients.split(',').map(ingredient => ingredient.trim());
+  
+    return recipe;
+  }
 
 
 
 exports.getRecipeDetails = getRecipeDetails;
 
 
+// the following code was written by me - Idan :
 
+function extractPreviewRecipeDetails(recipes_info) {
+  return recipes_info.map((recipe_info) => {
+      //check the data type so it can work with diffrent types of data
+      let data = recipe_info;
+      if (recipe_info.data) {
+          data = recipe_info.data;
+      }
+      const {
+          id,
+          title,
+          readyInMinutes,
+          image,
+          aggregateLikes,
+          vegan,
+          vegetarian,
+          glutenFree
+      } = data;
+      return {
+          id: id,
+          title: title,
+          image: image,
+          readyInMinutes: readyInMinutes,
+          popularity: aggregateLikes,
+          vegan: vegan,
+          vegetarian: vegetarian,
+          glutenFree: glutenFree
+      }
+  })
+}
+
+// We might not really need this function - possibly can be deleted.
+
+// function extractFullviewRecipeDetails(recipes_info) {
+//     return recipes_info.map((recipe_info) => {
+//         //check the data type so it can work with diffrent types of data
+//         let data = recipe_info;
+//         if (recipe_info.data) {
+//             data = recipe_info.data;
+//         }
+//         const {
+//             id,
+//             title,
+//             readyInMinutes,
+//             image,
+//             aggregateLikes,
+//             vegan,
+//             vegetarian,
+//             glutenFree,
+//             serving,
+//             instructions,
+//             extendedIngredients
+//         } = data;
+//         return {
+//             id: id,
+//             title: title,
+//             image: image,
+//             readyInMinutes: readyInMinutes,
+//             popularity: aggregateLikes,
+//             vegan: vegan,
+//             vegetarian: vegetarian,
+//             glutenFree: glutenFree,
+//             serving: serving,
+//             instructions: instructions,
+//             extendedIngredients: extendedIngredients
+//         }
+//     })
+//   }
+  
+
+
+async function getRecipesPreview(recipes_ids_list) {
+  let promises = [];
+  recipes_ids_list.map((id) => {
+      promises.push(getRecipeInformation(id));
+  });
+  let info_res = await Promise.all(promises);
+  //info_res.map((recp)=>{console.log(recp.data)});
+  info_res = extractPreviewRecipeDetails(info_res);
+  console.log(info_res);
+  return info_res
+}
+
+// async function getRecipesFullview(recipes_ids_list) {
+//     let promises = [];
+//     recipes_ids_list.map((id) => {
+//         promises.push(getRecipeInformation(id));
+//     });
+//     let info_res = await Promise.all(promises);
+//     info_res.map((recp)=>{console.log(recp.data)});
+//     info_res = extractFullviewRecipeDetails(info_res);
+//     console.log(info_res);
+//     return info_res
+//   }
+
+async function getRecipeFullviewDetails(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, serving, ingredients, instructions, extendedIngredients } = recipe_info.data;
+
+    // Map over the extendedIngredients to get the required fields
+    let simplifiedIngredients = extendedIngredients.map(ingredient => ({
+        name: ingredient.name,
+        amount: ingredient.amount,
+        unit: ingredient.unit
+    }));
+
+    return {
+        id: id,
+        title: title,
+        readyInMinutes: readyInMinutes,
+        image: image,
+        popularity: aggregateLikes,
+        vegan: vegan,
+        vegetarian: vegetarian,
+        glutenFree: glutenFree,
+        serving: serving,
+        ingredients: ingredients,
+        instructions: instructions,
+        extendedIngredients: simplifiedIngredients
+    };
+}
+
+
+//getRecipesPreview(["663559","642582","655705","652100"]);
+//getRecipesPreview(["663559","642582"]);
+
+// Add this line to export the new function
+exports.getRecipesPreview = getRecipesPreview;
+
+// Existing exports
+exports.getRecipeDetails = getRecipeDetails;
+exports.searchRecipe = searchRecipe;
+exports.extractPreviewRecipeDetails = extractPreviewRecipeDetails;
+//exports.getRecipesFullview = getRecipesFullview;
+exports.getRecipeFullviewDetails = getRecipeFullviewDetails;
+
+exports.getRecipesPreviewFromDb = getRecipesPreviewFromDb
+exports.getRecipeInformationFromDb =getRecipeInformationFromDb
